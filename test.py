c2ef9c8be18736a25b19a67b3164db887e167e4d
Shikhar-SRJ <ptksuraj@gmail.com>
2020-11-08 13:37:41 +0530

    for api testing-2

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 # import the necessary packages
 import requests
+import os
+import argparse
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--image', help='name of image to be tested', default='index.jpeg')
+args = parser.parse_args()
 
 # initialize the Keras REST API endpoint URL along with the input
 # image path
 KERAS_REST_API_URL = "http://localhost:5000/predict"
-IMAGE_PATH = "/home/suraj/Desktop/index.jpeg"
+IMAGE_PATH = BASEDIR + '/images/' +args.image
+# IMAGE_PATH = "/home/suraj/Desktop/index.jpeg"
+
 
 # load the input image and construct the payload for the request
 image = open(IMAGE_PATH, "rb").read()
@@ -12,6 +22,7 @@ payload = {"image": image}
 
 # submit the request
 r = requests.post(KERAS_REST_API_URL, files=payload).json()
+print(f'\n{r}\n\n')
 
 # ensure the request was successful
 if r["success"]:
