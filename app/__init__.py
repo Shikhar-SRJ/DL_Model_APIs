e32a7e57598a8066c645a75cc2f06f3a82cfc5a3
Shikhar-SRJ <ptksuraj@gmail.com>
2020-11-15 15:20:53 +0530

    added config

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
-app = Flask(__name__)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
 
